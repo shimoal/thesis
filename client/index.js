f57db2fae5227ce3a4598a0342e495984bcbd40e
hanyen <me@hanyen.com>
2017-02-17 17:23:42 -0800

    Implement basic nested components and links

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import ReactDOM from 'react-dom'
-import App from './Components/App.jsx'
+import App from './Components/App'
+import About from './Components/About'
+import Repos from './Components/Repos'
+import Repo from './Components/Repo'
+import Home from './Components/Home'
+import routes from './Components/routes'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app'));
