d90ce2c4c9db452a0a201534f36e86f4b56b266b
Alison Reed <alison.michelle.reed@gmail.com>
2017-02-23 15:08:34 -0800

    Finish socketSpec

--- server/socket.js
+++ server/socket.js
@@ -10,6 +10,8 @@ var userNum = 0;
 var rooms = {};
 
 io.on('connection', function(socket) {
+  userNum++;
+
   console.log('a user connected');
   socket.on('disconnect', function() {
     console.log('user disconnected');
@@ -17,12 +19,11 @@ io.on('connection', function(socket) {
     console.log('counter: ', userNum);
     // delete usernames[username];
   });
-  /** when the client emits 'adduser', this listens and executes **/
+  // /** when the client emits 'adduser', this listens and executes **/
   socket.on('adduser', function(username){
     // store the username in the socket session for this client
     // socket.username = username;
     // usernames[username] = username;
-    userNum++;
     console.log(username);
 
     console.log('counter: ', userNum);
@@ -41,13 +42,16 @@ io.on('connection', function(socket) {
         console.log('counter: ', userNum, '. rooms', JSON.stringify(rooms));      
     }
   });
-  /** when client emits 'join-room' */
+
+
+  // /** when client emits 'join-room' */
   socket.on('join-room', function(room_name) {
     socket.join(room_name);
     io.in(room_name).emit('welcome', 'a new user has joined the room');// broadcast to everyone in the room
       rooms[room_name]++;
       console.log('counter: ', userNum, '. rooms', JSON.stringify(rooms));
   });
+
 
   socket.on('editor-content-changes', function(room_name, val) {
     // io.emit will send to all client, socket.broadcast.emit will NOT send to sender
@@ -58,7 +62,7 @@ io.on('connection', function(socket) {
     io.in(room_name).emit('submit-val', val);
   });
 
-  /** for the video chat **/
+  // /** for the video chat **/
   socket.on('sendDescription', function(room_name, data) {
     socket.broadcast.to(room_name).emit('description', data);
   });
@@ -74,3 +78,4 @@ http.listen(port, function() {
   console.log('now listening on port ' + port);
 });
 
+// module.exports.server = http;
--- spec/socketSpec.js
+++ spec/socketSpec.js
@@ -1,74 +1,149 @@
 var chai = require('chai');
 var expect = chai.expect;
-var socket = require('socket.io-client');
+// var should = require('should');
+var io = require('socket.io-client');
+var client1;
+var client2;
 
 var options ={
-  transports: ['websocket'],
-  'force new connection': true
-};
+      transports: ['websocket'],
+      'force new connection': true
+    };
+
 
 describe('socket', function() {
 
-  it ('should exist', function() {
-    expect(socket).to.not.be.undefined;
+  beforeEach(function (done) {
+    client1 = io("http://localhost:8080", options);
+    done();
   });
 
-  var client1 = socket("http://localhost:8080", options);
-  var client2 = socket("http://localhost:8080", options);
+  afterEach(function (done) {
+    client1.disconnect();
+    client2.disconnect();
+    done();
+  });
 
-  it ('should increment users who connect', function() {
-    client2.on('newUser', function(numUsers){
-      numberOfUsers = numUsers
-      numberOfUsers.should.equal(2);
-      client2.disconnect();
+  it ('should give error message if room is already taken', function (done) {
+
+    client1.on('connect', function() {
+      client1.emit('addroom', 'roomA');
+      client2 = io("http://localhost:8080", options);
+
+      client2.on('connect', function() {
+        client2.emit('addroom', 'roomA');
+
+        client2.on('room-exists', function(msg) {
+          expect(msg).to.equal('The room name is taken, please try other names');
+          done();
+        })
+      });
+    })
+  });
+
+  it ('should give welcome message if room is not taken', function (done) {
+    client1.on('connect', function() {
+      client1.emit('addroom', 'roomB');
+
+      client1.on('welcome', function(msg) {
+        expect(msg).to.equal('a new user has joined the room');
+        done();
+      })
+
     });
   });
 
-  it ('should decrement users who disconnect', function() {
-    client1.on('newUser', function(numUsers) {
-     numUsers.should.equal(1);    
+  it ('should notify others in the room when a user joins', function (done) {
+    client1.on('connect', function() {
+      client1.emit('addroom', 'roomC');
+
+      client2 = io("http://localhost:8080", options);
+
+      client2.on('connect', function() {
+        client2.emit('join-room', 'roomC');
+
+        client1.on('welcome', function(msg) {
+          expect(msg).to.equal('a new user has joined the room');
+          done();
+        })
+      })
+    });
+  });
+
+
+  it ('should broadcast to all in the room when a change is made to the editor', function (done) {
+    client1.on('connect', function() {
+      client1.emit('addroom', 'roomD');
+      client2 = io('http://localhost:8080', options);
+
+      client2.on('connect', function() {
+        client2.emit('join-room', 'roomD');
+
+        client1.emit('editor-content-changes', 'roomD', 'changes to the editor');
+
+        client2.on('editor-content-changes', function(val) {
+          expect(val).to.equal('changes to the editor');
+          done();
+        });
+      });
+    });
+  });
+
+  it ('should broadcast submit value to others in room', function(done) {
+    client1.on('connect', function() {
+      client1.emit('addroom', 'roomE');
+
+      client2 = io('http://localhost:8080', options);
+
+      client2.on('connect', function() {
+        client2.emit('join-room', 'roomE');
+
+        client1.emit('submit-val', 'roomE', 'some value to submit');
+
+        client2.on('submit-val', function(val) {
+          expect(val).to.equal('some value to submit');
+          done();
+        });
+      });
+    });
+  });
+
+  it ('should send the description to peer connection', function(done) {
+    client1.on('connect', function() {
+      client1.emit('addroom', 'roomF');
+
+      client2 = io('http://localhost:8080', options);
+
+      client2.on('connect', function() {
+        client2.emit('join-room', 'roomF');
+
+        client1.emit('sendDescription', 'roomF', 'description for remote video');
+
+        client2.on('description', function(desc) {
+          expect(desc).to.equal('description for remote video');
+          done();
+        });
+      });
+    });
+  });
+
+  it ('should send the ice Candidates to peer connection', function(done) {
+    client1.on('connect', function() {
+      client1.emit('addroom', 'roomG');
+
+      client2 = io('http://localhost:8080', options);
+
+      client2.on('connect', function() {
+        client2.emit('join-room', 'roomG');
+
+        client1.emit('sendCandidate', 'roomG', 'ice candidate');
+
+        client2.on('candidate', function(can) {
+          expect(can).to.equal('ice candidate');
+          done();
+        });
+      });
     });
   });
 
 });
-
-describe('sendDescription', function() {
-
-  var client1 = socket("http://localhost:8080", options);
-  var client2 = socket("http://localhost:8080", options);
-
-  it ('should send sendDescription event', function() {
-    client1.emit('sendDescription', 'Example description');
-    client1.on('sendDescription', function(data) {
-      data.should.equal('Example description');
-    });
-  });
-
-  it ('should broadcast description event', function() {
-    client1.emit('sendDescription', 'Example description');
-    client2.on('description', function(data) {
-      data.should.equal('Example description');
-    });
-  });
-
-
-});
-
-describe('sendCandidate', function() {
-  var client1 = socket("http://localhost:8080", options);
-  var client2 = socket("http://localhost:8080", options);
-
-  it ('should send sendCandidate event', function() {
-    client1.emit('sendCandidate', 'Example candidate');
-    client1.on('sendCandidate', function(data) {
-      data.should.equal('Example candidate');
-    });
-  });
-
-  it ('should broadcast candidate event', function() {
-    client1.emit('sendCandidate', 'Example candidate');
-    client2.on('candidate', function(data) {
-      data.should.equal('Example candidate');
-    });
-  });
-})
