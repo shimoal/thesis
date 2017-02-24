c70777a5390bf71bc8ef4f2ad13c283c02a64c5b
Alison Reed <alison.michelle.reed@gmail.com>
2017-02-24 12:03:16 -0800

    Removed console logs from deployed version of collaborate

--- client/Components/Collaborate.js
+++ client/Components/Collaborate.js
@@ -137,24 +137,19 @@ export default class Collaborate extends React.Component {
 
 	/********* video conference *********/
 	start(isCaller) {
-
-		console.log('this.state in caller', this.state.room_name);
 		var room_name = this.state.room_name;
+
 	  //pc will be created for both caller and answerer
 	  pc = new RTCPeerConnection(configuration);
-		console.log('set pc: ', pc);
+
 
 		// send any ice candidates to the other peer
 	  pc.onicecandidate = function (evt) {
-	    console.log('send ice candidates:', evt);
-		  // signalingChannel.emit('sendCandidate', (JSON.stringify({ "candidate": evt.candidate })));
 		  socket.emit('sendCandidate', room_name, (JSON.stringify({ "candidate": evt.candidate })));
 	  };
 
 		// once remote stream arrives, show it in the remote video element
-	  pc.onaddstream = function (evt) {
-	    console.log('adding remote stream');		
-      console.log('event: ', evt);      
+	  pc.onaddstream = function (evt) {      
 	    $("#peer-camera video")[0].src = URL.createObjectURL(evt.stream);
 	  };
 
@@ -172,7 +167,6 @@ export default class Collaborate extends React.Component {
 
 		  function gotDescription(desc) {
 		    pc.setLocalDescription(desc);
-	      // signalingChannel.emit('sendDescription', JSON.stringify({ "sdp": desc }));
 	      socket.emit('sendDescription', room_name, JSON.stringify({ "sdp": desc }));
 	    };
 	  });
@@ -193,7 +187,6 @@ export default class Collaborate extends React.Component {
 		  this.start(false);      
 	  }
     var description = (JSON.parse(evt)).sdp;
-		console.log('setting remote description');
 	  pc.setRemoteDescription(new RTCSessionDescription(description));
 	}
 	handleCandidate(evt) {
@@ -201,7 +194,6 @@ export default class Collaborate extends React.Component {
         this.start(false);
       }
       var candidate = (JSON.parse(evt)).candidate;
-      console.log('adding recieved ice candidates')
       pc.addIceCandidate(new RTCIceCandidate(candidate));
 	}
 	/************************************/	
