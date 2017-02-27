f1030414d68c045684d5536b250cc678706d9af5
Alison Reed <alison.michelle.reed@gmail.com>
2017-02-26 18:13:12 -0800

    Save user name on session

--- server/auth/githubAuth.js
+++ server/auth/githubAuth.js
@@ -1,17 +1,9 @@
 var githubAuth = require('./githubAuthConfig.js');
 var passport = require('passport');
 var GithubStrategy = require('passport-github2').Strategy;
-<<<<<<< HEAD
-<<<<<<< HEAD
 var usersCtrl = require('./../db/users/usersController.js');
 var User = require('./../db/users/usersModel.js');
 var sess;
-=======
->>>>>>> Add github authentication
-=======
-var usersCtrl = require('./../db/users/usersController.js');
-var sess;
->>>>>>> More work on authentication
 
 var app = require('./../app.js');
 var githubCB
@@ -31,33 +23,21 @@ passport.use(new GithubStrategy({
     callbackURL: githubCB
   },
   function(accessToken, refreshToken, profile, done) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     console.log('insdie Strategy cb', profile);
     var user = {name: profile.displayName, email: profile.emails[0].value, profile_img: profile._json.avatar_url, github_id: profile.id}
     console.log('user:', user);
     return done(null, user);
-=======
-    return done(null, profile);
->>>>>>> Add github authentication
-=======
-    console.log('insdie Strategy cb');
-    var user = {name: profile.displayName, email: profile.emails[0].value}
-    return done(null, user);
->>>>>>> More work on authentication
   }
 ));
 
 
 passport.serializeUser(function(user, done) {
-<<<<<<< HEAD
   // null is for errors
   console.log('inside serializeUser', user);
   done(null, user);
 });
 
 passport.deserializeUser(function(name, done) {
-=======
   // placeholder for custom user serialization
   // console.log('information inside serializeUser:');
   // console.log('id: ', user.id);
@@ -65,28 +45,8 @@ passport.deserializeUser(function(name, done) {
   // console.log('name: ', user.displayName);
   // null is for errors
   console.log('inside serializeUser', user);
-
   done(null, user);
 });
-
-<<<<<<< HEAD
-passport.deserializeUser(function(user, done) {
->>>>>>> Add github authentication
-=======
-passport.deserializeUser(function(name, done) {
->>>>>>> More work on authentication
-  // placeholder for custom user deserialization.
-  // maybe you are going to get the user from mongo by id?
-  // null is for errors
-  // console.log('deserializeUser', user);
-<<<<<<< HEAD
-<<<<<<< HEAD
-  console.log('inside deserializeUser', user);
-  done(null, user);
-
-
-});
-
 
 var githubAuth = {
   checkUser: function(req, res, next) {
@@ -135,80 +95,6 @@ var githubAuth = {
         return res.redirect('/dashboard');
       }
     });
-
-=======
-  done(null, user);
-=======
-  console.log('inside deserializeUser', user);
-  if (registeredUsers[name]) {
-    console.log('insdie registeredUsers', registeredUsers);
-    done(null, user);
-  }
-
->>>>>>> More work on authentication
-});
-
-var registeredUsers = {};
-
-var githubAuth = {
-  checkUser: function(req, res, next) {
-    if(sess) {
-      console.log('user has a sess')
-      // console.log('sess:', sess);
-      next('user has a sess');
-    } else {
-      console.log('setting user session');
-      sess = req.session;
-      next();
-    }
-  },
-
-  authenticate: function(req, res, next) {
-
-    console.log('authenticate req', sess);
-
-    if (sess && sess.username) {
-      console.log('authenticated');
-      next(sess.username);
-    } else {
-      console.log('not authenticated');
-      res.redirect('/login');
-    }
-  },
-
-  failureRedirect: passport.authenticate('github', { failureRedirect: '/' }),
-
-  successCallback: function(req, res, next) {
-    console.log('inside successCallback');
-<<<<<<< HEAD
-    var session = req.session;
-    session.username = req.user.displayName;
-    console.log('session:', session.username);
-    res.redirect('/');
->>>>>>> Add github authentication
-=======
-
-    sess.username = req.user.name;
-    // console.log(sess);
-    registeredUsers[req.user.name] = true;
-    req.logIn(req.user, function(err) {
-      if (err) {
-        next(err);
-      } else {
-        return res.redirect('/');
-      }
-    });
-    // res.redirect('/');
-    // console.log(req.cookieID);
-    // req.cookie.cookieName = 'user';
-
-    // req.cookie.username = req.user.displayName;
-
-    // console.log('cookie:', req.cookie.username);
-    // req.session['username'] = req.user.displayName;
-    // next();
-    // res.redirect('/');
->>>>>>> More work on authentication
   }
 };
 
