c887edf5d9c2b807e1154e2f6ced4eccf8bb1244
Alison Reed <alison.michelle.reed@gmail.com>
2017-02-28 20:34:17 -0800

    Add options for development environment

--- server/auth/githubAuthConfig.js
+++ server/auth/githubAuthConfig.js
@@ -1,5 +1,15 @@
-const CLIENT_ID = '85106edf149ff2c63847';
-const CLIENT_SECRET = 'bef136331482c2a10c0efd1d05c98d0461147a75';
+var app = require('./../app.js');
+
+var CLIENT_ID;
+var CLIENT_SECRET;
+
+if ( app.get('env') === 'development' ) {
+  CLIENT_ID = '476dca26b617b2c27a23';
+  CLIENT_SECRET = 'd9a82187bf260d1f78211e403b5c1892d3283e41';
+} else {
+  CLIENT_ID = '85106edf149ff2c63847';
+  CLIENT_SECRET = 'bef136331482c2a10c0efd1d05c98d0461147a75';
+}
 
 module.exports.CLIENT_ID = CLIENT_ID;
 module.exports.CLIENT_SECRET = CLIENT_SECRET;
