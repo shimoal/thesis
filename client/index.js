a8e329f2b79ac21b5272de2b17fff49bf933685c
Alison Reed <alison.michelle.reed@gmail.com>
2017-03-01 15:31:56 -0800

    Include video chat in collaborate

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import ReactDOM from 'react-dom'
 import routes from './Components/routes'
-import NavLink from './Components/NavLink'
 
 ReactDOM.render(
   <Router routes={routes} history={browserHistory}/>,
