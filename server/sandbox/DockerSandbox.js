52731e3fae101906826aac6cfcb76215eb6ad492
Ai Shi <shiai8901@gmail.com>
2017-03-02 19:15:03 -0800

    added a docker sandbox

--- server/sandbox/DockerSandbox.js
+++ server/sandbox/DockerSandbox.js
@@ -3,5 +3,8 @@ var DockerSandbox = function(timeout_value, vm_name, code) {
 	this.vm_name = vm_name;
 	this.compiler_name = "nodejs";
 	this.code = code;
-
 }
+
+DockerSandbox.prototype.run = function() {
+	
+};
