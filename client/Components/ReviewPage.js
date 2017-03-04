651c6db055d771c1c440f6738a8055c0f8d17d13
Ai Shi <shiai8901@gmail.com>
2017-03-03 16:13:54 -0800

    created a review table model

--- client/Components/ReviewPage.js
+++ client/Components/ReviewPage.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
 export default React.createClass {
+
+	/****************************/
+	/* props = {
+		learnerId: learnerId,
+		helperId: helperId,
+		id: questionId,
+		content: questionContent
+		}
+	/*
+	/****************************/
+
 	render() {
 		return (
 			<div className="container-fluid">
 				<div className="row">
-					<LeftColumn />
+					<LeftColumn 
+						userCurrent={this.props.userData.user} 
+						question={this.props.userData}
+						/>
 					<RightColumn />
 				</div>
 			</div>
