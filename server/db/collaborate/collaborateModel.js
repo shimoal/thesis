232a69e4f1d9de58e4522e2da8afbcbd6e96cb73
Ai Shi <shiai8901@gmail.com>
2017-03-04 14:03:21 -0800

    updated collaborate table model

--- server/db/collaborate/collaborateModel.js
+++ server/db/collaborate/collaborateModel.js
@@ -1,23 +1,20 @@
 const Sequelize = require('sequelize');
 const db = require('../database.js');
-// const User = require('../users/usersModel.js');
+const User = require('../users/usersModel.js');
+const Question = require('../questions/questionsModel.js');
+const Review = require('../reviews/reviewsModel.js');
 
 const Collaborate = db.define('collaborate', {
-  id_learner: {
-    type: Sequelize.INTEGER
-  },
-  id_helper: {
-    type: Sequelize.INTEGER
-  },
-  id_question: {
-    type: Sequelize.INTEGER
-  }, 
   room_number: {
     type: Sequelize.STRING
-  }, 
+  }
 });
 
 // User.hasMany(Question);
+Collaborate.belongsTo(User, {foreignKey: 'id_learner', as: 'Learner'});
+Collaborate.belongsTo(User, {foreignKey: 'id_helper', as: 'Helper'});
+Collaborate.belongsTo(Question, {foreignKey: 'id_question', as: 'Question'});
+Collaborate.belongsTo(Review, {foreignKey: 'id_review'});
 
 db.sync();
 
