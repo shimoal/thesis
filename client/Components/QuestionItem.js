716ea61c791e4f278ea20a8fd13a2d7303cdc1ab
hanyen <me@hanyen.com>
2017-03-06 15:29:35 -0800

    Working on fixing the claim button on homepage

--- client/Components/QuestionItem.js
+++ client/Components/QuestionItem.js
@@ -42,10 +42,10 @@ var QuestionItem = React.createClass({
   // },
 
   isAuthenticated: function() {
-    if (this.props.authenticated !== undefined &&
+    if (!!this.props.userData &&
         this.props.details.helperId !== undefined && 
         this.props.userCurrent.id !== undefined) {
-      if (this.props.authenticated === 1 && this.props.details.helperId !== this.props.userCurrent.id) {
+      if (!!this.props.userData && this.props.details.helperId !== this.props.userCurrent.id) {
         console.log('===== condition met');
         return (
           <ClaimQuestionButton 
