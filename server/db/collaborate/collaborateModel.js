d41417446328ef25b6fed186afae13a42e55da24
Ai Shi <shiai8901@gmail.com>
2017-03-06 19:34:46 -0800

    fixed model

--- server/db/collaborate/collaborateModel.js
+++ server/db/collaborate/collaborateModel.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('../database.js');
 const User = require('../users/usersModel.js');
 const Question = require('../questions/questionsModel.js');
+const Claim = require('../claims/claimsModel.js');
 const Review = require('../reviews/reviewsModel.js');
 
 const Collaborate = db.define('collaborate', {
@@ -10,20 +11,20 @@ const Collaborate = db.define('collaborate', {
   }
 });
 
-// User.hasMany(Question);
 Collaborate.belongsTo(User, {foreignKey: 'id_learner', as: 'Learner'});
 Collaborate.belongsTo(User, {foreignKey: 'id_helper', as: 'Helper'});
 Collaborate.belongsTo(Question, {foreignKey: 'id_question', as: 'Question'});
-Collaborate.belongsTo(Review, {foreignKey: 'id_review', as: 'Review'});
-// Review.belongsTo(Collaborate, {foreignKey: 'id_collaborate', as: 'Collaborate'});
+// Collaborate.belongsTo(Review, {foreignKey: 'id_review', as: 'Review'});
+Review.belongsTo(Collaborate, {foreignKey: 'id_collaborate', as: 'Collaborate'});
 
 db.sync()
 // .then(function() {
 //   return Collaborate.create({
 //       id_learner: 1,
 //       id_helper: 2,
-//       id_question: 3,
-//       room_number: 1234,
+//       id_question: 1,
+//       room_number: '1234',
+//       id_review: 1
 //     })  
 // })
 // .then(function() {
@@ -33,7 +34,6 @@ db.sync()
 //     .then(function(collaborates) {
 //       collaborates.forEach(function(collaborate) {
 //         console.log(collaborate.get());
-
 //       })
 //       console.log("************ create collaborate successfully *************")
 //     });
