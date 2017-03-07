fb08f32464b1ab6ec02d170bcce607c40e36130c
Ai Shi <shiai8901@gmail.com>
2017-03-06 19:35:11 -0800

    fixed model, added association with collaborate table

--- server/db/reviews/reviewsModel.js
+++ server/db/reviews/reviewsModel.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 const db = require('../database.js');
+const User = require('../users/usersModel.js');
+const Question = require('../questions/questionsModel.js');
+const Claim = require('../claims/claimsModel.js');
 const Collaborate = require('../collaborate/collaborateModel.js');
 
 const Review = db.define('review', {
@@ -13,24 +16,23 @@ const Review = db.define('review', {
 		type: Sequelize.INTEGER
 	},
 	content: {
-		type: Sequelize.STRING
+		type: Sequelize.TEXT
 	}
 });
 
 // Review.belongsTo(Collaborate, {foreignKey: 'id_collaborate', as: 'Collaborate'});
 
-// Collaborate.belongsTo(Review, {foreignKey: 'id_review', as: 'Review'});
-
 db.sync()
-// .then(function() {
-// 	return Review.create({
-// 		knowledge: 5,
-// 		helpfulness: 5,
-// 		overall: 5,
-// 		content: 'good helper',
-// 		id_collaborate: 6,
-// 	})
-// })
+.then(function() {
+	return Review.create({
+		knowledge: 5,
+		helpfulness: 5,
+		overall: 5,
+		content: 'good helper',
+		id_collaborate: 1,
+	})
+	console.log("model has been made")
+})
 .catch(function(err) {
 	console.log("err in review model", err.message);
 });
