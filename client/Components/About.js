53fb7b54f45e723ec50cfa3d73965ed424168275
Alison Reed <alison.michelle.reed@gmail.com>
2017-03-07 17:27:20 -0800

    Add directions in list form

--- client/Components/About.js
+++ client/Components/About.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default React.createClass({
   render() {
-    return <div><h2>How it works</h2></div>
+    return (<div>
+      <h1>How it works</h1>
+      <h2> What would you like to do? </h2>
+      <h3>Ask a question</h3>
+      <p>1. If you haven't already, create an account!</p>
+      <p>2. Post a question on any topic you'd like help with</p>
+      <p>3. Browse questions, or use the search bar to find a question you'd like to answer</p>
+      <p>4. If you find a quesiont, click "Claim" to offer to answer</p>
+      <p>5. Once someone has claimed your questions, you can choose to accept their help if you want </p>
+      <p>6. As soon as you accept, you will both be sent an email with your roomnumber </p>
+      <p>7. Go to the collaborate page, and enter the room to immediately connect and start coding! </p>
+    </div>)
   }
 })
