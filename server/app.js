46f901f2cfc8028e0991d1174acf4536c20a341e
Alison Reed <alison.michelle.reed@gmail.com>
2017-03-09 14:13:00 -0800

    restart

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    maxAge: 1000 * 60 * 60 * 5 
+    maxAge: 1000 * 60 * 60 * 6 
   } //cookie expires in 5 hours
 }));
 
