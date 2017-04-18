48053c88867c47bb3569d2e1f03bedf5e7e66149
Ai Shi <shiai8901@gmail.com>
2017-04-17 17:57:05 -0700

    added conditional rendering the review ratings, need to replace text with start-rating components

--- client/Components/HelperProfileItem.js
+++ client/Components/HelperProfileItem.js
@@ -1,25 +1,87 @@
 import React from 'react';
 import { Link } from 'react-router';
+import ReactStars from 'react-stars'
 
 export default React.createClass({
 
   constructProfileLink() {
-    // return 'profile/' + this.props.details.github_id;
     return 'profile/' + this.props.details.id;
   },
 
   render() {
+
+    console.log('this.props.details', this.props.details);
+
     return (
       <div className="col-xs-6 col-sm-3 placeholder">
         <img src={this.props.details.profile_img} width="200px"/>
         <h3><Link to={this.constructProfileLink()}>{this.props.details.name}</Link></h3>
         
         <p>&nbsp;</p>
-        <p>Javascript (5)</p>
-        <p>HTML (5)</p>
-        <p>CSS (5)</p>
+        {this.props.details.reviewRatings !== undefined ? (
+          <div>
+            <p>averageOverall: {this.props.details.reviewRatings.averageOverall}</p>
+            <p>averageKnowledge: {this.props.details.reviewRatings.averageKnowledge}</p>
+            <p>averageHelpfulness: {this.props.details.reviewRatings.averageHelpfulness}</p>
+            <p>averageExperience: {this.props.details.reviewRatings.averageExperience}</p>
+          </div> 
+          ) : (
+          <p>no reviews</p>
+          )
+        }
         
+
       </div>
     );
   }
 });
+// <div className="table-responsive">
+        //       <table className="table">
+        //         <tbody>
+        //           <tr>
+        //             <td>Overall</td>
+        //             <td>
+        //               <ReactStars
+        //               value={this.state.overall}
+        //                 count={5}
+        //                 half={false}
+        //                 edit={false}/>
+        //             </td>
+        //           </tr>
+        //           <tr>
+        //             <td>&nbsp;</td>
+        //             <td>&nbsp;</td>
+        //           </tr>
+        //           <tr>
+        //             <td>Knowledge</td>
+        //             <td>
+        //               <ReactStars
+        //               value={this.state.knowledge}
+        //                 count={5}
+        //                 half={false}
+        //                 edit={false}/>
+        //             </td>
+        //           </tr>
+        //           <tr>
+        //             <td>Helpfulness</td>
+        //             <td>
+        //               <ReactStars
+        //               value={this.state.helpfulness}
+        //                 count={5}
+        //                 half={false}
+        //                 edit={false}/>
+        //             </td>
+        //           </tr>
+        //           <tr>
+        //             <td>Overall Experience</td>
+        //             <td>
+        //               <ReactStars
+        //               value={this.state.experience}
+        //                 count={5}
+        //                 half={false}
+        //                 edit={false}/>
+        //             </td>
+        //           </tr>
+        //         </tbody>
+        //       </table>
+        //     </div>
